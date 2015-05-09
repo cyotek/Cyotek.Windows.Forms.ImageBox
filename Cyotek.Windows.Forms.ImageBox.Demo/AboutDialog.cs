337cad347fbbee7c5a0eeaf2622f1f07a8d7ef09
Richard Moss <cyotek@cyotek.com>
2015-05-09 18:25:57 +0100

    Added NuGet Package Restore
    Added NuGet Package Restore, and updated packages used by the demo to
    the latest versions.

--- Cyotek.Windows.Forms.ImageBox.Demo/AboutDialog.cs
+++ Cyotek.Windows.Forms.ImageBox.Demo/AboutDialog.cs
@@ -5,7 +5,7 @@ using System.IO;
 using System.Reflection;
 using System.Windows.Forms;
 using CommonMark;
-using HtmlRenderer;
+using TheArtOfDev.HtmlRenderer.WinForms;
 
 namespace Cyotek.Windows.Forms.Demo
 {
