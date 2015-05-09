ca5e2e74728eb19a1f242e0aa816466ecbc98592
Richard Moss <cyotek@cyotek.com>
2015-05-09 18:41:06 +0100

    Version fix

--- Cyotek.Windows.Forms.ImageBox/Properties/AssemblyInfo.cs
+++ Cyotek.Windows.Forms.ImageBox/Properties/AssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("d812a438-008c-47f9-926f-8415490cdda1")]
 [assembly: CLSCompliant(true)]
-[assembly: AssemblyVersion("1.1.5.0")]
+[assembly: AssemblyVersion("1.1.5.1")]
 [assembly: AssemblyFileVersion("1.1.5.1")]
