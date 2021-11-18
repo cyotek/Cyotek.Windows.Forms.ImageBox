7387496658c32898781de6af8e3442aacb7382e2
Richard Moss <richard.moss@cyotek.com>
2021-11-18 19:32:44 +0000

    SDK Pass 1: Converted projects to SDK style
    * Solution builds, demonstration seems to run ok
    * Referenced documentation files broken
    * Packages updated to latest version

--- Cyotek.Windows.Forms.ImageBox/Properties/AssemblyInfo.cs
+++ Cyotek.Windows.Forms.ImageBox/Properties/AssemblyInfo.cs
@@ -2,17 +2,9 @@ using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Cyotek ImageBox Control")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Cyotek Ltd")]
-[assembly: AssemblyProduct("Cyotek ImageBox Control")]
-[assembly: AssemblyCopyright("Copyright © 2010-2020 Cyotek Ltd.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("d812a438-008c-47f9-926f-8415490cdda1")]
 [assembly: CLSCompliant(true)]
 [assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
-[assembly: AssemblyInformationalVersion("1.2.2")]
